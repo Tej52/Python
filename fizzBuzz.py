9ebfb502867c02b5ae5757e5155a2fcdac5cf4bd
Developer Tej <shivatej.developer@gmail.com>
2024-09-24 00:25:19 +0530

    Fizzbuzz program

---
+++ fizzBuzz.py
@@ -0,0 +1,12 @@
+num = int(input('Enter a positive integer : '))
+
+for x in range(1,num+1):
+    if x%3 == 0 and x%5 == 0:
+        print('FizzBuzz')
+    elif x%3 == 0:
+        print('Fizz')
+    elif x%5 == 0:
+        print('Buzz')
+    else:
+        print(x)
+        

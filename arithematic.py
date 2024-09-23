08f6e4b48f68dcbb2c8b0d28e8b35944a3215358
Developer Tej <shivatej.developer@gmail.com>
2024-09-24 00:24:41 +0530

    Arithmetic operators

---
+++ arithematic.py
@@ -0,0 +1,17 @@
+num1 = int(input("Enter First integer : "))
+num2 = int(input("Enter second integer : "))
+
+# Addition
+print(f"\nSum of {num1} and {num2} is {num1+num2}\n")
+
+# Substraction
+print(f"Difference between {num1} and {num2} is {num1-num2}\n")
+
+# Multiplication
+print(f"Product of {num1} and {num2} is {num1*num2}\n")
+
+# Division
+print(f"Quotient of {num1} and {num2} is {num1/num2}\n")
+
+# Floor Division
+print(f"Floor division of {num1} and {num2} is {num1//num2}")

a6729993ee8daf891051b3b57120bfc2b13185ac
Developer Tej <shivatej.developer@gmail.com>
2024-09-26 00:59:11 +0530

    Program to find the second largest element in the given list

---
+++ secondLargest.py
@@ -0,0 +1,18 @@
+list = []
+length = int(input("Enter number of Elements : "))
+
+for i in range(length):
+    temp = int(input("Enter the number : "))
+    list.append(temp)
+    
+#  Negative indexing
+
+else:
+    list.sort()
+    print(list[-2])
+
+#  Positive indexing
+
+# else:
+#     list.sort(reverse=True)
+#     print(list[1])
